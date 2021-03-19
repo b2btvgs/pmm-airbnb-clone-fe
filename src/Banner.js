0bcd70d25b495bf3e003ca7c5022b9f0df632b7d
Paul Mccranie <paulmccranie@Pauls-MacBook-Pro.local>
2021-03-19 16:11:14 -0500

    initial commit airbnb-clone - built along with watching Clever Programmer YouTube series

---
+++ src/Banner.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import Search from "./Search";
+import Button from "@material-ui/core/Button";
+import "./Banner.css";
+
+function Banner() {
+  const [showSearch, setShowSearch] = useState(false);
+  const history = useHistory();
+  return (
+    <div className="banner">
+      <div className="banner__search">
+        {showSearch && <Search />}
+        <Button
+          variant="outlined"
+          className="banner__searchButton"
+          onClick={() => setShowSearch(!showSearch)}
+        >
+          {showSearch ? "Hide" : "Search Dates"}
+        </Button>
+      </div>
+      <div className="banner__info">
+        <h1>Get out and stretch your imagination</h1>
+        <h5>
+          Plan a different kind of getaway to uncover the hidden gems near you
+        </h5>
+        <Button onClick={() => history.push("/search")} variant="outlined">
+          Explore Nearby
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
